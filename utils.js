8270403124232e0eea82873336eb722eb7cb83e2
CyberVy <72680847+CyberVy@users.noreply.github.com>
2022-02-23 03:09:44 +0800

    Create utils.js

---
+++ utils.js
@@ -0,0 +1,27 @@
+function createVideo Element(src,poster=""){
+
+var v = document.createElement("video")
+v.src=src
+var width=screen.availWidth
+v.setAttribute("width", width)
+v.setAttribute("height",width/16*9)
+v.setAttribute("poster",poster)
+v.setAttribute("autoplay","")
+v.setAttribute("controls","")
+v.setAttribute("muted","")
+v.setAttribute("playsinline","")
+return v
+
+}
+
+
+function createElement(tag,dict){
+
+var tag = document.createElement(tag)
+
+for (let item in dict){
+tag.setAttribute(item,dict[item])
+}
+
+return tag
+}
